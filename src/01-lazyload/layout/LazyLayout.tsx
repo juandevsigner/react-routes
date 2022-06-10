375618ea52fab137e2cfbc5d93170e3ad0f7d287
Juan Devsigner <juanda.core07@gmail.com>
2022-06-10 10:32:01 -0500

    heading change

--- src/01-lazyload/layout/LazyLayout.tsx
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -4,7 +4,7 @@ import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
 const LazyLayout = () => {
   return (
     <div>
-      <h1>LazyLayout Pages</h1>
+      <h1>LazyLayout Page</h1>
       <ul>
         <li>
           <NavLink to="lazy1">Lazy 1</NavLink>
